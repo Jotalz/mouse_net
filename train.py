e713a9e31ee6202b55c99d9b814dd6051ef2a931
Yotal <zjtisangel@gmail.com>
2024-03-13 15:35:32 +0800

    将部分常用项添加为输入修改

--- train.py
+++ train.py
@@ -13,8 +13,9 @@ from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
+
 class Data:
-    def __init__(self, delete_prev_data = True):
+    def __init__(self, delete_prev_data=True):
         self.data_path = './data.txt'
         self.data_queue = queue.Queue()
         if delete_prev_data:
@@ -22,31 +23,35 @@ class Data:
         self.thread = threading.Thread(target=self.write_to_file, name='Data')
         self.running = True
         self.thread.start()
-        
+
     def write_to_file(self):
         while self.running:
             target = self.data_queue.get()
             if target is None:
                 break
             with open(self.data_path, 'a') as f:
-                f.write(f'{target[0]} {target[1]} {target[2]} {target[3]} {target[4]} {target[5]} {target[6]} {target[7]} {target[8]} {target[9]} {target[10]} {target[11]}\n')
-    
+                f.write(
+                    f'{target[0]} {target[1]} {target[2]} {target[3]} {target[4]} {target[5]} {target[6]} {target[7]} {target[8]} {target[9]} {target[10]} {target[11]}\n')
+
     def delete_data(self):
         if Option_delete_prev_data:
             try:
                 os.remove(self.data_path)
-            except: pass
+            except:
+                pass
             try:
                 os.remove('./mouse_net.pth')
-            except: pass
+            except:
+                pass
 
     def add_target_data(self, target):
         self.data_queue.put(target)
-        
+
     def stop(self):
         self.running = False
-        self.data_queue.put(None) # call break
+        self.data_queue.put(None)  # call break
         self.thread.join()
+
 
 class Game_settings:
     def __init__(self):
@@ -58,28 +63,31 @@ class Game_settings:
         self.fov_y = Option_fov_y
         self.mouse_dpi = Option_mouse_dpi
         self.mouse_sensitivity = Option_mouse_sensitivity
-    
+
     def randomize(self):
         if Option_random_screen_resolution:
             prev_screen_width = self.screen_width
             prev_screen_height = self.screen_height
-            self.screen_width = random.randint(Option_random_screen_resolution_width[0], Option_random_screen_resolution_width[1])
-            self.screen_height = random.randint(Option_random_screen_resolution_height[0], Option_random_screen_resolution_height[1])
+            self.screen_width = random.randint(Option_random_screen_resolution_width[0],
+                                               Option_random_screen_resolution_width[1])
+            self.screen_height = random.randint(Option_random_screen_resolution_height[0],
+                                                Option_random_screen_resolution_height[1])
             self.update_target_position(prev_screen_width, prev_screen_height)
-            
+
         self.screen_x_center = int(self.screen_width / 2)
         self.screen_y_center = int(self.screen_height / 2)
-        
+
         if Option_random_fov:
             self.fov_x = random.randint(Option_random_fov_x[0], Option_random_fov_x[1])
             self.fov_y = random.randint(Option_random_fov_y[0], Option_random_fov_y[1])
-            
+
         if Option_random_mouse_dpi:
             self.mouse_dpi = random.randint(Option_random_mouse_dpi_min_max[0], Option_random_mouse_dpi_min_max[1])
-            
+
         if Option_random_mouse_sensitivity:
-            self.mouse_sensitivity = random.uniform(Option_random_mouse_sensitivity_min_max[0], Option_random_mouse_sensitivity_min_max[1])
-        
+            self.mouse_sensitivity = random.uniform(Option_random_mouse_sensitivity_min_max[0],
+                                                    Option_random_mouse_sensitivity_min_max[1])
+
     def update_target_position(self, prev_width, prev_height):
         scale_x = self.screen_width / prev_width
         scale_y = self.screen_height / prev_height
@@ -89,12 +97,13 @@ class Game_settings:
         target.h = min(target.h, self.screen_height)
         target.prev_x = int(target.prev_x * scale_x)
         target.prev_y = int(target.prev_y * scale_y)
-        
+
+
 class Target:
     def __init__(self, x, y, w, h, dx, dy):
         w = min(w, game_settings.screen_width)
         h = min(h, game_settings.screen_height)
-        
+
         self.x = x + w // 2
         self.y = y + h // 2
         self.w = w
@@ -123,31 +132,31 @@ class Target:
         if self.y - self.h // 2 < 0:
             self.y = self.h // 2
             self.dy = -self.dy
-            
+
     def randomize_size(self):
         max_width = game_settings.screen_width
         max_height = game_settings.screen_height
         self.w = random.randint(4, max_width)
         self.h = random.randint(4, max_height)
-        
+
     def randomize_position(self):
         self.w = min(self.w, game_settings.screen_width)
         self.h = min(self.h, game_settings.screen_height)
         self.x = random.randint(self.w // 2, game_settings.screen_width - self.w // 2)
         self.y = random.randint(self.h // 2, game_settings.screen_height - self.h // 2)
-        
+
     def randomize_velocity(self):
         self.dx += random.uniform(Option_gen_speed_x[0], Option_gen_speed_x[1])
         self.dy += random.uniform(Option_gen_speed_y[0], Option_gen_speed_y[1])
 
         self.dx = max(-1, min(self.dx, 1))
         self.dy = max(-1, min(self.dy, 1))
-        
+
     def get_velocity(self):
         return self.dx, self.dy
 
     def get_speed(self):
-        return math.sqrt(self.dx**2 + self.dy**2)
+        return math.sqrt(self.dx ** 2 + self.dy ** 2)
 
     def get_direction(self):
         return math.atan2(self.dy, self.dx)
@@ -158,10 +167,10 @@ class Target:
     def predict_target_position(self, target_x, target_y, target_w, target_h, game_settings):
         velocity_x = target_x - self.prev_x
         velocity_y = target_y - self.prev_y
-        
+
         predicted_x = target_x + velocity_x
         predicted_y = target_y + velocity_y
-        
+
         predicted_x = max(target_w // 2, min(predicted_x, game_settings.screen_width - target_w // 2))
         predicted_y = max(target_h // 2, min(predicted_y, game_settings.screen_height - target_h // 2))
 
@@ -171,14 +180,14 @@ class Target:
         mouse_move_x, mouse_move_y = self.adjust_mouse_movement(predicted_x, predicted_y, game_settings)
 
         return mouse_move_x, mouse_move_y
-    
+
     def adjust_mouse_movement(self, target_x, target_y, game_settings):
         offset_x = target_x - game_settings.screen_x_center
         offset_y = target_y - game_settings.screen_y_center
 
         degrees_per_pixel_x = game_settings.fov_x / game_settings.screen_width
         degrees_per_pixel_y = game_settings.fov_y / game_settings.screen_height
-        
+
         mouse_move_x = offset_x * degrees_per_pixel_x
 
         mouse_dpi_move_x = (mouse_move_x / 360) * (game_settings.mouse_dpi * (1 / game_settings.mouse_sensitivity))
@@ -187,7 +196,8 @@ class Target:
         mouse_dpi_move_y = (mouse_move_y / 360) * (game_settings.mouse_dpi * (1 / game_settings.mouse_sensitivity))
 
         return mouse_dpi_move_x, mouse_dpi_move_y
-        
+
+
 class Visualisation(threading.Thread):
     def __init__(self):
         super(Visualisation, self).__init__()
@@ -195,26 +205,29 @@ class Visualisation(threading.Thread):
         self.cv2_window_name = 'train_mouse_net'
         self.running = True
         self.start()
-    
+
     def run(self):
-        cv2.namedWindow(self.cv2_window_name) 
+        cv2.namedWindow(self.cv2_window_name)
         while self.running:
             image = np.zeros((game_settings.screen_height, game_settings.screen_width, 3), np.uint8)
-            
+
             try:
                 data = self.queue.get(timeout=0.1)
             except queue.Empty:
                 continue
             if data is None:
                 break
-            
+
             if Option_gen_visualise_draw_line:
-                x, y = target.predict_target_position(target_x=target.x, target_y=target.y, target_w=target.w, target_h=target.h, game_settings=game_settings)
-                cv2.line(image, (int(game_settings.screen_x_center), int(game_settings.screen_y_center)), (int(data.x + x), int(data.y + y)), (0, 255, 255), 2)
-            
-            cv2.rectangle(image, (int(data.x - data.w // 2), int(data.y - data.h // 2)), (int(data.x + data.w // 2), int(data.y + data.h // 2)), (0, 255, 0), 2)
+                x, y = target.predict_target_position(target_x=target.x, target_y=target.y, target_w=target.w,
+                                                      target_h=target.h, game_settings=game_settings)
+                cv2.line(image, (int(game_settings.screen_x_center), int(game_settings.screen_y_center)),
+                         (int(data.x + x), int(data.y + y)), (0, 255, 255), 2)
+
+            cv2.rectangle(image, (int(data.x - data.w // 2), int(data.y - data.h // 2)),
+                          (int(data.x + data.w // 2), int(data.y + data.h // 2)), (0, 255, 0), 2)
             cv2.imshow(self.cv2_window_name, image)
-            
+
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         cv2.destroyAllWindows()
@@ -224,7 +237,8 @@ class Visualisation(threading.Thread):
         self.running = False
         self.queue.put(None)
         self.join()
-    
+
+
 class CustomDataset(Dataset):
     def __init__(self, filepath):
         self.data = []
@@ -240,6 +254,7 @@ class CustomDataset(Dataset):
         inputs, targets = self.data[idx]
         return torch.tensor(inputs, dtype=torch.float32), torch.tensor(targets, dtype=torch.float32)
 
+
 class Mouse_net(nn.Module):
     def __init__(self):
         super(Mouse_net, self).__init__()
@@ -254,7 +269,8 @@ class Mouse_net(nn.Module):
         x = torch.relu(self.fc3(x))
         x = self.fc4(x)
         return x
-    
+
+
 def format_time(seconds):
     hours = int(seconds // 3600)
     minutes = int((seconds % 3600) // 60)
@@ -262,6 +278,7 @@ def format_time(seconds):
 
     formatted_time = f"{hours:02}:{minutes:02}:{sec:06.3f}"
     return formatted_time
+
 
 def train_net():
     save_path = 'models/'
@@ -277,9 +294,9 @@ def train_net():
 
     epochs = Option_train_epochs
     loss_values = []
-    
+
     start_time = time.time()
-    
+
     for epoch in range(epochs):
         epoch_losses = []
         for inputs, targets in dataloader:
@@ -291,18 +308,18 @@ def train_net():
             optimizer.step()
             epoch_losses.append(loss.item())
             last_update_time = time.time()
-        
+
         epoch_loss = np.mean(epoch_losses)
         loss_values.append(epoch_loss)
-        
-        train_time = last_update_time - start_time
-        
-        print(f'Epoch {epoch+1}/{epochs}', 'Loss: {:.5f}'.format(epoch_loss), format_time(train_time))
 
-        if (epoch+1) % Option_save_every_N_epoch == 0:
-            torch.save(model.state_dict(), os.path.join(save_path, f'mouse_net_epoch_{epoch+1}.pth'))
-            print(f'Model saved at epoch {epoch+1}')
-    
+        train_time = last_update_time - start_time
+
+        print(f'Epoch {epoch + 1}/{epochs}', 'Loss: {:.5f}'.format(epoch_loss), format_time(train_time))
+
+        if (epoch + 1) % Option_save_every_N_epoch == 0:
+            torch.save(model.state_dict(), os.path.join(save_path, f'mouse_net_epoch_{epoch + 1}.pth'))
+            print(f'Model saved at epoch {epoch + 1}')
+
     plt.plot(loss_values)
     plt.title('Loss over epochs')
     plt.xlabel('Epoch')
@@ -310,6 +327,7 @@ def train_net():
     plt.show()
 
     torch.save(model.state_dict(), 'mouse_net.pth')
+
 
 def test_net():
     print('Starting testing model...')
@@ -330,12 +348,13 @@ def test_net():
             predictions.append(prediction.cpu().numpy())
             actuals.append(targets.cpu().numpy())
 
-    mse = np.mean((np.array(predictions) - np.array(actuals))**2)
+    mse = np.mean((np.array(predictions) - np.array(actuals)) ** 2)
     print(f"Mean Squared Error on Test Data: {mse}")
+
 
 def gen_data():
     pbar = tqdm(total=Option_gen_time, desc='Data generation')
-    
+
     global target
     target = Target(
         x=random.randint(0, game_settings.screen_width),
@@ -347,7 +366,7 @@ def gen_data():
 
     start_time = time.time()
     last_update_time = time.time()
-    
+
     while True:
         game_settings.randomize()
         current_time = time.time()
@@ -359,101 +378,107 @@ def gen_data():
         target.randomize_size()
         target.randomize_position()
         target.randomize_velocity()
-        
+
         if Option_gen_visualise:
             vision.queue.put(target)
-        
-        x, y = target.predict_target_position(target_x=target.x, target_y=target.y, target_w=target.w, target_h=target.h, game_settings=game_settings)
-        
+
+        x, y = target.predict_target_position(target_x=target.x, target_y=target.y, target_w=target.w,
+                                              target_h=target.h, game_settings=game_settings)
+
         data.add_target_data((game_settings.screen_width,
-                                game_settings.screen_height,
-                                game_settings.screen_x_center,
-                                game_settings.screen_y_center,
-                                game_settings.mouse_dpi,
-                                game_settings.mouse_sensitivity,
-                                game_settings.fov_x,
-                                game_settings.fov_y,
-                                target.x,
-                                target.y,
-                                x,
-                                y))
+                              game_settings.screen_height,
+                              game_settings.screen_x_center,
+                              game_settings.screen_y_center,
+                              game_settings.mouse_dpi,
+                              game_settings.mouse_sensitivity,
+                              game_settings.fov_x,
+                              game_settings.fov_y,
+                              target.x,
+                              target.y,
+                              x,
+                              y))
         pbar.n = int(last_update_time - start_time)
         pbar.refresh()
 
         if int(last_update_time - start_time) >= Option_gen_time:
             if Option_gen_visualise:
-                vision.queue.put(None) # call break
+                vision.queue.put(None)  # call break
             data.stop()
             pbar.close()
             break
-        
+
+
 if __name__ == "__main__":
+    delete_pre = input("是否删除之前的数据和模型(y/n)：")
+    train = input("是否启用训练(y/n)：")
+    generate = input("是否生成训练数据(y/n)：")
+    vision = input("是否启用训练可视化(y/n)：")
+    test = input("是否启用测试(y/n)：")
     ###################### Options ######################
-    
     # Game settings
     Option_screen_width = 384
     Option_screen_height = 216
     Option_fov_x = 90
-    Option_fov_y = 55
-    Option_mouse_dpi = 1000
+    Option_fov_y = 59
+    Option_mouse_dpi = 800
     Option_mouse_sensitivity = 2
-    
+
     # Data
-    Option_delete_prev_data = True
-    
+    Option_delete_prev_data = False if delete_pre in ["0", "n", "no", "N", "NO"] else True
+
     # Train
-    Option_train = True
+    Option_train = False if train in ["0", "n", "no", "N", "NO"] else True
     Option_train_epochs = 10
-    Option_train_batch_size = 2048 * 4 # I'm too lazy to type 4 digits, so I just multiply
+    Option_train_batch_size = 2048 * 4  # I'm too lazy to type 4 digits, so I just multiply
     Option_save_every_N_epoch = 10
-    
+
     # Generation settings
-    Option_Generation = True
+    Option_Generation = False if generate in ["0", "n", "no", "N", "NO"] else True
     Option_gen_time = 160
-    
-    Option_gen_visualise = False
+
+    Option_gen_visualise = False if vision in ["0", "n", "no", "N", "NO"] else True
     Option_gen_visualise_draw_line = True
-    
+
     # Speed - 1 is max
     Option_gen_speed_x = [-1, 1]
     Option_gen_speed_y = [-1, 1]
-    
+
     # Game settings - random options
     Option_random_screen_resolution = False
     Option_random_screen_resolution_width = [400, 580]
     Option_random_screen_resolution_height = [300, 420]
-    
+
     Option_random_fov = False
     Option_random_fov_x = [80, 90]
     Option_random_fov_y = [55, 70]
-    
+
     Option_random_mouse_dpi = False
     Option_random_mouse_dpi_min_max = [1000, 3000]
-    
+
     Option_random_mouse_sensitivity = False
     Option_random_mouse_sensitivity_min_max = [1, 3]
-    
+
     # Testing model
-    Option_test_model = False
+    Option_test_model = False if test in ["0", "n", "no", "N", "NO"] else True
 
     #####################################################
 
     game_settings = Game_settings()
-    
+
     data = Data(delete_prev_data=Option_delete_prev_data)
-    
+
     if Option_gen_visualise:
         vision = Visualisation()
-    
+
     if Option_train or Option_test_model:
         device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    
+
     if Option_Generation:
-       gen_data()
-        
+        gen_data()
+
     if Option_gen_visualise:
         vision.stop()
-    
+
     if Option_train:
         train_net()
 
